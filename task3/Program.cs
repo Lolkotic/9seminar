c00a0cf89cddb1586bb62641323127cf6cddaa3a
Lola <lola@example.com>
2022-10-14 00:12:10 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+
+Console.WriteLine("Введите наименьшее из двух чисел");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите наибольшее из двух чисел");
+int M = Convert.ToInt32(Console.ReadLine());
+
+
+void SumOfDigitsBetweenNM(int N, int M, int sum)
+    {
+        if (N > M) 
+        {
+            Console.WriteLine($"Сумма натуральных чисел между M и  N = {sum}");
+        return;
+        }
+        sum = sum +(N++);
+        SumOfDigitsBetweenNM(N,M,sum);
+    }
+SumOfDigitsBetweenNM(N,M,0);
+
+
+
+
